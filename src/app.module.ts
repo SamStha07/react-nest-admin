a3956601b9ecdfc42caef91e57046871b54d6916
samstha07 <samshrestha85@gmail.com>
2021-12-13 11:50:35 +0545

    working on register

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './user/models/user..entity';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'admin',
+      entities: [User],
+      synchronize: true, //true is only used in development
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
